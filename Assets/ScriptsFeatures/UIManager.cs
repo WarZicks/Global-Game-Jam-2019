413e3ec0d073d5883dc082ea3a7422355177070a
TomRivaud <temtonik@gmail.com>
2019-01-27 17:10:50 +0100

    thty

--- Assets/ScriptsFeatures/UIManager.cs
+++ Assets/ScriptsFeatures/UIManager.cs
@@ -7,6 +7,8 @@ public class UIManager : MonoBehaviour {
 
     public GameObject myGameObjectText;
     public Text myText;
+    public GameObject imageNewt;
+    public GameObject nameNewt;
 
     // Use this for initialization
     void Start() {
@@ -22,59 +24,98 @@ public class UIManager : MonoBehaviour {
     {
         myText.text = "A new thing to put in my house !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
+
     }
 
     public void BlueItemUI()
     {
         myText.text = "A new thing to put in my house !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void GreenItemUI()
     {
         myText.text = "A new thing to put in my house !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void WoodItemUI()
     {
         myText.text = "Yes a stick !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void RareWoodItemUI()
     {
         myText.text = "Yay a rare stick !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void DarkRockItemUI()
     {
         myText.text = "Yes a dark stone !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void GoldRockItemUI()
     {
         myText.text = "Yay a golden stone !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void StandardFishItemUI()
     {
         myText.text = "Yes I caught a fish !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void RareFishItemUI()
     {
         myText.text = "Yay I caught a rare fish!";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
     }
 
     public void GoldFishItemUI()
     {
         myText.text = "Ouah it's a golden fish !";
         myGameObjectText.SetActive(true);
+        imageNewt.SetActive(true);
+        nameNewt.SetActive(true);
+        StartCoroutine(TimeToDisable());
+    }
+
+    IEnumerator TimeToDisable()
+    {
+        yield return new WaitForSeconds(2f);
+        myGameObjectText.SetActive(false);
+        imageNewt.SetActive(false);
+        nameNewt.SetActive(false);
     }
 }
